ddc9b9b88ed1f2723a44de472bb5ff38c66c1fe2
Stcwal <stwalmann@outlook.com>
2025-04-03 18:57:48 +0200

    naob reader

--- src/app/cv/mdfiles/contact.md
+++
@@ -1,7 +0,0 @@
-### Email
-
-[stwalmann@outlook.com](stwalmann@outlook.com)
-
-### Phone
-
-+47 953 36 953
--- src/app/projects/components/scrape-example.tsx
+++ src/app/projects/components/scrape-example.tsx
@@ -1,6 +1,7 @@
 import Markdown, { getMarkdownContent } from "@/components/markdown";
 import { Suspense } from "react";
 import { HtmlReader } from "./html-reader";
+import { NaobReader } from "./naob-reader";
 
 const content = getMarkdownContent("src/app/projects/mdfiles/wordscrape.md");
 
@@ -15,13 +16,20 @@ export default function WordScrape() {
       </Suspense>
       <div className="rounded-lg border border-gray-300 shadow-md bg-white w-1/2 mx-auto mt-6">
         <div className="w-1/2 h-[50vh] mx-auto mb-4 items-center justify-center">
-          <p className="text-black">
-            Kansje en video ellerno?
-            {/* <HtmlReader url={new URL(`https://naob.no/ordbok/fotball`)} /> */}
-          </p>
+          <p className="text-black">Kansje en video ellerno?</p>
+          <HtmlToText />
         </div>
         |
       </div>
+    </div>
+  );
+}
+
+export function HtmlToText() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold mb-4">HTML to Text Preview</h1>
+      {/* <NaobReader url="https://naob.no/ordbok/fotball" /> */}
     </div>
   );
 }
