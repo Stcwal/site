211dcfa6aea4a8553be22117b319ea7380f6a46f
Stcwal <stwalmann@outlook.com>
2025-04-03 07:15:22 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import NavigationHeader from "../components/header";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import InfoFooter from "@/components/footer";
 
 export default function Home() {
   return (
     <div>
       <NavigationHeader />
-      <div className="p-4 w-auto text-center">
-        <ScrollArea>
-          <h1 className="font-sans text-2xl">Welcome</h1>
-          <a className="font-sans">This is my eportfolio !</a>
+      <div className="flex-1 p-4 w-full flex flex-col items-center justify-center min-h-[calc(100vh-2*64px)]">
+        <ScrollArea className="w-full max-w-3xl">
+          <div className="text-center">
+            <h1 className="font-sans text-2xl mb-2">Welcome</h1>
+            <p className="font-sans">This is my eportfolio !</p>
+          </div>
         </ScrollArea>
       </div>
+      <InfoFooter />
     </div>
   );
 }
