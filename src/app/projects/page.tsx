f221da74b4bd597f9d0eb36a0b10b3a970e333bd
Stcwal <stwalmann@outlook.com>
2025-04-13 15:55:58 +0200

    fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,3 @@
-import NavigationHeader from "@/components/header";
 import WordScrape from "./components/scrape-example";
 
 export const dynamic = "force-dynamic";
@@ -8,5 +7,5 @@ export default function ProjectsPage({
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  return <WordScrape searchParams={searchParams} />;
+  return <WordScrape searchParams={searchParams || {}} />;
 }
